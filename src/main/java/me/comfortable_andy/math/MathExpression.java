debae5c59767c425b0a3e6de59f8fe0c7a540d92
AndyNoob <comfortableandy9082@gmail.com>
2023-12-02 00:19:03 -0800

    Added check for empty parts list in MathExpression#evaluate
    Signed-off-by: AndyNoob <comfortableandy9082@gmail.com>

--- src/main/java/me/comfortable_andy/math/MathExpression.java
+++ src/main/java/me/comfortable_andy/math/MathExpression.java
@@ -47,6 +47,7 @@ public class MathExpression {
         */
 
         // first ensure list is valid
+        if (this.parts.isEmpty()) throw new IllegalStateException("Empty parts list");
         if (!isValid()) throw new IllegalStateException("Invalid expression");
 
         // then evaluate all evaluable parts
